4bccf618ca471946a4f74d356a07f3d553c5d782
Rob Cameron <rcameron@riotgames.com>
2016-01-21 10:17:37 -0800

    updating init

--- init.el
+++ init.el
@@ -79,31 +79,14 @@
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 (setq gofmt-command "goimports")
 (require 'golint)
-;; pickup the path from the shell
-;(defun set-exec-path-from-shell-PATH ()
-;  (let ((path-from-shell (replace-regexp-in-string
-;                          "[ \t\n]*$"
-;                          ""
-;                         (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-;   (setenv "PATH" path-from-shell)
-;   (setq eshell-path-env path-from-shell) ; for eshell users
-;   (setq exec-path (split-string path-from-shell path-separator))))
-
-;when window-system (set-exec-path-from-shell-PATH))
 
 ; run gofmt on save
-;(setq exec-path (cons "/Users/rcameron/gopath/bin" exec-path))
-;(add-hook 'before-save-hook 'gofmt-before-save)
-
-
-;; run gofmt on save
-
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 ;; godef
-;; (defun my-go-mode-hook ()
-  ; Call gofmt before savingb
-  ; (add-hook 'before-save-hook 'gofmt-before-save)
-  ; Godef jump key binding
-  ;(local-set-key (kbd "M-.") 'godef-jump))
-
-;(add-hook 'go-mode-hook 'my-go-mode-hook)
+(defun my-go-mode-hook ()
+;Call gofmt before savingb
+  (add-hook 'before-save-hook 'gofmt-before-save)
+   ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+(add-hook 'go-mode-hook 'my-go-mode-hook))
